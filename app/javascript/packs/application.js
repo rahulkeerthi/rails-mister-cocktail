a9310d3a016daf333cbb4121faa85b450dec41d1
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-02-21 15:46:17 +0000

    sweetalert2 issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import swal from 'sweetalert';
 import 'bootstrap';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
